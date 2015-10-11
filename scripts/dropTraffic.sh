acd0e959fb5d3269a2d47bb11c47cea19af278aa
Krzysztof <krzysztof.otrebski@gmail.com>
2015-10-11 20:26:25 +0200

    drop scripts

--- scripts/dropTraffic.sh
+++ scripts/dropTraffic.sh
@@ -9,13 +9,18 @@ function usage(){
 
 if [[ "$#" -eq 2 && "$1" == "drop" ]]
   then
-   echo "Will drop $2 packets"
+   echo "Will drop $2 input and output packets"
+   # for randomly dropping 10% of incoming packets:
+   iptables -A INPUT -m statistic --mode random --probability $2 -j DROP
+   # and for dropping 10% of outgoing packets:
+   iptables -A OUTPUT -m statistic --mode random --probability $2 -j DROP
    exit 0
 fi
 
 if [[ "$#" -eq 1 && "$1" == "clear" ]]
  then
- echo "Clear drop config" 
+ echo "Clear drop config"
+ iptables -F
  exit 0
 fi  
 
