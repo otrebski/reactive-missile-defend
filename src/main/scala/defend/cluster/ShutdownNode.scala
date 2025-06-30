f4293e8ba47bb3426ad6423034ad154d83b7c36b
Krzysztof Otrębski <krzysztof.otrebski@gmail.com>
2025-06-30 08:25:01 +0200

    Update ShutdownNode.scala

--- src/main/scala/defend/cluster/ShutdownNode.scala
+++ src/main/scala/defend/cluster/ShutdownNode.scala
@@ -5,6 +5,8 @@ import akka.cluster.Cluster
 import akka.cluster.sharding.ShardRegion
 import pl.project13.scala.rainbow._
 
+import scala.concurrent.{ Await, Awaitable, CanAwait }
+import scala.concurrent.duration.FiniteDuration
 import scala.language.postfixOps
 
 trait ShutdownNode {
@@ -29,17 +31,24 @@ trait ShutdownNode {
         println("   Have received command to leave cluster    ".onWhite.red)
         println("   Trying to do graceful leave               ".onWhite.red)
         println("                                             ".onRed)
-        context.watch(towerShard)
-        towerShard ! ShardRegion.GracefulShutdown
+        val cluster: Cluster = Cluster(system)
+        cluster.leave(cluster.selfAddress)
+        val awaitable = new Awaitable[Boolean] {
+          override def ready(atMost: Duration)(implicit permit: CanAwait): this.type = this
+
+          override def result(atMost: Duration)(implicit permit: CanAwait): Boolean = cluster.isTerminated
+        }
+        Await.ready(awaitable, 30 seconds)
+      
 
       case Terminated(`towerShard`) ⇒
         println("                                             ".onRed)
         println("   Cluster region was terminated             ".onWhite.red)
         println("   Terminate and leave                       ".onWhite.red)
         println("                                             ".onRed)
-        val cluster: Cluster = Cluster(system)
-        system.terminate()
         system.registerOnTermination(System.exit(0))
+        system.terminate()
+
       case ShutdownNode.SystemExit0 =>
         println("                                              ".onRed)
         println("   Have received command to System.exit(0)    ".onWhite.red)
