cce62a25f3388e3b34059b45526082faa66ea5ed
Krzysztof Otrębski <krzysztof.otrebski@gmail.com>
2025-06-30 08:28:35 +0200

    Start scripts added

---
+++ start_cc_2506.sh
@@ -0,0 +1,6 @@
+export SEED_NODE=akka.tcp://defend@127.0.0.1:4001
+export SEED_NODE2=akka.tcp://defend@127.0.0.1:4000
+export HOST=127.0.0.1
+export PORT=2506
+export CASSANDRA_HOST=127.0.0.1
+./target/universal/missiledefend-1.0.2/bin/missiledefend cc
